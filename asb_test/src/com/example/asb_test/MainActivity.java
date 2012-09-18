32d187f0c1ded81e6f676578aea4d34ed80d6834
gfreese <gfreese@727f26b2-5668-4d6c-8cd6-724b558d2b0f>
2012-09-18 14:08:50 +0000

    Added SVG support, changed home screen image (temporarily ignoring linear layout on main activity)
    git-svn-id: https://subversion.assembla.com/svn/aiha_android/trunk@22 727f26b2-5668-4d6c-8cd6-724b558d2b0f

--- asb_test/src/com/example/asb_test/MainActivity.java
+++ asb_test/src/com/example/asb_test/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.asb_test;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.app.ActionBar;
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
 
 public class MainActivity extends SherlockFragmentActivity implements ActionBar.TabListener
 {
@@ -19,19 +23,16 @@ public class MainActivity extends SherlockFragmentActivity implements ActionBar.
 		setTheme(THEME);
 		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		
 		
 
+		//configure actionbar to hide logo and use tabs
 		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
 		getSupportActionBar().setDisplayUseLogoEnabled(false);
 		getSupportActionBar().setDisplayShowTitleEnabled(false);
 		getSupportActionBar().setDisplayShowHomeEnabled(false);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
-		
+		//create tabs for actionbar
 		ActionBar.Tab newTab0 = getSupportActionBar().newTab().setText("AIHA");
 		ActionBar.Tab newTab1 = getSupportActionBar().newTab().setText("Convert");
 		ActionBar.Tab newTab2 = getSupportActionBar().newTab().setText("Noise");
@@ -39,14 +40,12 @@ public class MainActivity extends SherlockFragmentActivity implements ActionBar.
 		ActionBar.Tab newTab4 = getSupportActionBar().newTab().setText("Ventilation");
 		ActionBar.Tab newTab5 = getSupportActionBar().newTab().setText("Exposure");
 		
-		
 		newTab0.setTabListener(this);
 		newTab1.setTabListener(this);
 		newTab2.setTabListener(this);
 		newTab3.setTabListener(this);
 		newTab4.setTabListener(this);
 		newTab5.setTabListener(this);
-		
 
 		getSupportActionBar().addTab(newTab0);
 		getSupportActionBar().addTab(newTab1);
@@ -54,6 +53,22 @@ public class MainActivity extends SherlockFragmentActivity implements ActionBar.
 		getSupportActionBar().addTab(newTab3);
 		getSupportActionBar().addTab(newTab4);
 		getSupportActionBar().addTab(newTab5);
+		
+		//temporary: display AIHA image on home screen
+		
+        
+        ImageView imageView = new ImageView(this);	// Create a new ImageView
+        
+        //imageView.setBackgroundColor(Color.RED);	// Set the background color to white
+       
+        SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.aiha_tag_white); // Parse the SVG file from the resource
+        
+        imageView.setImageDrawable(svg.createPictureDrawable());// Get a drawable from the parsed SVG and set it as the drawable for the ImageView
+        
+		setContentView(imageView);// Set the ImageView as the content view for the Activity
+		
+		
+		
 		
 
 	}
