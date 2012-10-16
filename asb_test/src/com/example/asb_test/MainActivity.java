1f6154321e72ecf25e972b40d88034b109b984ed
gfreese <gfreese@727f26b2-5668-4d6c-8cd6-724b558d2b0f>
2012-10-16 02:38:47 +0000

    * Tab fragment working
    git-svn-id: https://subversion.assembla.com/svn/aiha_android/trunk@25 727f26b2-5668-4d6c-8cd6-724b558d2b0f

--- asb_test/src/com/example/asb_test/MainActivity.java
+++ asb_test/src/com/example/asb_test/MainActivity.java
@@ -1,97 +1,197 @@
 package com.example.asb_test;
 
-import android.graphics.Color;
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TabHost;
 
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.app.ActionBar;
+
 import com.larvalabs.svgandroid.SVG;
 import com.larvalabs.svgandroid.SVGParser;
 
-public class MainActivity extends SherlockFragmentActivity implements ActionBar.TabListener
+public class MainActivity extends SherlockFragmentActivity
 {
-	public static int THEME = R.style.Theme_Aiha_Light;
 	
+	ViewPager mViewPager;
+	TabsAdapter mTabsAdapter;
+	public static int THEME = R.style.Theme_Aiha_Light;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		setTheme(THEME);
-		
+
 		super.onCreate(savedInstanceState);
-		
 
-		//configure actionbar to hide logo and use tabs
-		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		getSupportActionBar().setDisplayUseLogoEnabled(false);
-		getSupportActionBar().setDisplayShowTitleEnabled(false);
-		getSupportActionBar().setDisplayShowHomeEnabled(false);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+		ActionBar bar = getSupportActionBar();
+		setContentView(R.layout.activity_main);
+		mViewPager = (ViewPager) findViewById(R.id.pager);
 
-		//create tabs for actionbar
-		ActionBar.Tab newTab0 = getSupportActionBar().newTab().setText("AIHA");
-		ActionBar.Tab newTab1 = getSupportActionBar().newTab().setText("Convert");
-		ActionBar.Tab newTab2 = getSupportActionBar().newTab().setText("Noise");
-		ActionBar.Tab newTab3 = getSupportActionBar().newTab().setText("Heat");
-		ActionBar.Tab newTab4 = getSupportActionBar().newTab().setText("Ventilation");
-		ActionBar.Tab newTab5 = getSupportActionBar().newTab().setText("Exposure");
+		// configure actionbar to hide logo and use tabs
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		bar.setDisplayUseLogoEnabled(false);
+		bar.setDisplayShowTitleEnabled(false);
+		bar.setDisplayShowHomeEnabled(false);
+		bar.setDisplayHomeAsUpEnabled(false);
 		
-		newTab0.setTabListener(this);
-		newTab1.setTabListener(this);
-		newTab2.setTabListener(this);
-		newTab3.setTabListener(this);
-		newTab4.setTabListener(this);
-		newTab5.setTabListener(this);
+		mTabsAdapter = new TabsAdapter(this, mViewPager);
 
-		getSupportActionBar().addTab(newTab0);
-		getSupportActionBar().addTab(newTab1);
-		getSupportActionBar().addTab(newTab2);
-		getSupportActionBar().addTab(newTab3);
-		getSupportActionBar().addTab(newTab4);
-		getSupportActionBar().addTab(newTab5);
-		
-		//temporary: display AIHA image on home screen
-		
-        
-        ImageView imageView = new ImageView(this);	// Create a new ImageView
-        
-        //imageView.setBackgroundColor(Color.RED);	// Set the background color to white
-       
-        SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.aiha_tag_white); // Parse the SVG file from the resource
-        
-        imageView.setImageDrawable(svg.createPictureDrawable());// Get a drawable from the parsed SVG and set it as the drawable for the ImageView
-        
-		setContentView(imageView);// Set the ImageView as the content view for the Activity
-		
-		
-		
-		
+		// create tabs for actionbar		
+		mTabsAdapter.addTab("aiha", "AIHA",					EquationFragment.class, null);
+		mTabsAdapter.addTab("convert", "Convert",			EquationFragment.class, null);
+		mTabsAdapter.addTab("noise", "Noise",				EquationFragment.class, null);
+		mTabsAdapter.addTab("heat", "Heat",					EquationFragment.class, null);
+		mTabsAdapter.addTab("ventilation", "Ventilation",	EquationFragment.class, null);
+		mTabsAdapter.addTab("exposure", "Exposure",			EquationFragment.class, null);
 
+		//display AIHA SVG image
+		//ImageView imageView = new ImageView(this);
+		// imageView.setBackgroundColor(Color.RED); // Set the background color
+		//SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.aiha_tag_white);
+		//imageView.setImageDrawable(svg.createPictureDrawable());
+		//setContentView(imageView);
 	}
 
-	@Override
-	public void onTabReselected(Tab tab, FragmentTransaction ft)
-	{
-	}
 
-	@Override
-	public void onTabSelected(Tab tab, FragmentTransaction ft)
+	public static class TabsAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener,
+			ActionBar.TabListener
 	{
-		toastText("tab " + String.valueOf(tab.getPosition()) + " clicked");
-	}
+		private final SherlockFragmentActivity mContext;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
 
-	@Override
-	public void onTabUnselected(Tab tab, FragmentTransaction ft)
-	{
-	}
+		static final class TabInfo
+		{
+			@SuppressWarnings("unused")
+			private final String tag;
+			private final Class<?> clss;
+			private final Bundle args;
 
-	private void toastText(String message)
-	{
-		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+			TabInfo(String _tag, Class<?> _class, Bundle _args)
+			{
+				tag = _tag;
+				clss = _class;
+				args = _args;
+			}
+		}
+
+		static class DummyTabFactory implements TabHost.TabContentFactory
+		{
+			private final Context mContext;
+
+			public DummyTabFactory(Context context)
+			{
+				mContext = context;
+			}
+
+			@Override
+			public View createTabContent(String tag)
+			{
+				View v = new View(mContext);
+				v.setMinimumWidth(0);
+				v.setMinimumHeight(0);
+				return v;
+			}
+		}
+
+		public TabsAdapter(SherlockFragmentActivity activity, ViewPager pager)
+		{
+			super(activity.getSupportFragmentManager());
+			mContext = activity;
+			mViewPager = pager;
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
+
+		public void addTab(String tag, CharSequence label, Class<?> clss, Bundle args)
+		{
+			ActionBar.Tab tab = mContext.getSupportActionBar().newTab();
+			tab.setText(label);
+			tab.setTabListener(this);
+			mContext.getSupportActionBar().addTab(tab);
+			TabInfo info = new TabInfo(tag, clss, args);
+			mTabs.add(info);
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public int getCount()
+		{
+			return mTabs.size();
+		}
+
+		@Override
+		public Fragment getItem(int position)
+		{
+			TabInfo info = mTabs.get(position);
+			return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+		{
+		}
+
+		@Override
+		public void onPageSelected(int position)
+		{
+			mContext.getSupportActionBar().setSelectedNavigationItem(position);
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int state)
+		{
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.actionbarsherlock.app.ActionBar.TabListener#onTabSelected(com
+		 * .actionbarsherlock.app.ActionBar.Tab,
+		 * android.support.v4.app.FragmentTransaction)
+		 */
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft)
+		{
+			mViewPager.setCurrentItem(mContext.getSupportActionBar().getSelectedNavigationIndex());
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.actionbarsherlock.app.ActionBar.TabListener#onTabUnselected(com
+		 * .actionbarsherlock.app.ActionBar.Tab,
+		 * android.support.v4.app.FragmentTransaction)
+		 */
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft)
+		{
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.actionbarsherlock.app.ActionBar.TabListener#onTabReselected(com
+		 * .actionbarsherlock.app.ActionBar.Tab,
+		 * android.support.v4.app.FragmentTransaction)
+		 */
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft)
+		{
+		}
 	}
 
 }
