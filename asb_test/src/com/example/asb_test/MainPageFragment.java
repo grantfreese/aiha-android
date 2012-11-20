a99d5f3a77abf8a87d1ab78939701143f31569ae
dhubbard <dhubbard@727f26b2-5668-4d6c-8cd6-724b558d2b0f>
2012-11-20 03:01:04 +0000

    Added main page image fragment.
    git-svn-id: https://subversion.assembla.com/svn/aiha_android/trunk@40 727f26b2-5668-4d6c-8cd6-724b558d2b0f

---
+++ asb_test/src/com/example/asb_test/MainPageFragment.java
@@ -0,0 +1,30 @@
+package com.example.asb_test;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.graphics.Color;
+
+public class MainPageFragment extends SherlockFragment{
+	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View V = inflater.inflate(R.layout.test, container, false);
+        ImageView imageView = (ImageView)V.findViewById(R.id.AIHAtestImage);
+        imageView.setBackgroundColor(Color.BLUE); // Set the background color
+		SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.aiha_tag_white);
+		imageView.setImageDrawable(svg.createPictureDrawable());
+        return V;
+	}
+	
+
+}//end class
