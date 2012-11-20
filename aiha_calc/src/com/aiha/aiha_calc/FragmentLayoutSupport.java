143626c3f6f63877728c1e8924a443d4acaedfa0
dhubbard <dhubbard@727f26b2-5668-4d6c-8cd6-724b558d2b0f>
2012-11-20 09:43:17 +0000

    Added variable entry screen and layout.
    git-svn-id: https://subversion.assembla.com/svn/aiha_android/trunk@50 727f26b2-5668-4d6c-8cd6-724b558d2b0f

--- aiha_calc/src/com/aiha/aiha_calc/FragmentLayoutSupport.java
+++ aiha_calc/src/com/aiha/aiha_calc/FragmentLayoutSupport.java
@@ -10,13 +10,18 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.TableRow.LayoutParams;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -712,14 +717,46 @@ public class FragmentLayoutSupport extends SherlockFragmentActivity
 				return null;
 			}
 
-			ScrollView scroller = new ScrollView(getActivity());
-			TextView text = new TextView(getActivity());
-			int padding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getActivity().getResources().getDisplayMetrics());
-			 text.setPadding(padding, padding, padding, padding);
-			 scroller.addView(text);
-			 //text.setText(_equationList.getVect().elementAt(getShownIndex()).graphic_name); //temporary just to display *something*
-
-			return scroller;
+			  View V = inflater.inflate(R.layout.var_test, container, false);
+		        int numberOfFields = 3;
+		        
+		        TableLayout tl = (TableLayout)V.findViewById(R.id.varTable);
+		        LayoutParams params = new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1f);
+				
+				for(int i=0; i < numberOfFields; i++){
+					TableRow tr = new TableRow(V.getContext());
+					tr.setWeightSum(3);
+					tr.setGravity(Gravity.CENTER_HORIZONTAL);
+					tr.setId(1000+i);
+					
+					TextView tv1 = new TextView(V.getContext());
+					tl.setColumnStretchable(0, true);
+					tv1.setId(100+i);
+					tv1.setGravity(Gravity.RIGHT);
+					tv1.setText("Variable " + (i+1) + " ");
+					tv1.setLayoutParams(params);
+					tr.addView(tv1);
+				
+					EditText textbox1 = new EditText(V.getContext());
+					tl.setColumnStretchable(1, true);
+					textbox1.setId(10+i);
+					textbox1.setGravity(Gravity.RIGHT);
+					textbox1.setLayoutParams(params);
+					tr.addView(textbox1);
+				
+					TextView tv1f = new TextView(V.getContext());
+					tl.setColumnStretchable(2, true);
+					tv1f.setId(200+i);
+					tv1f.setText(" Unit " + (i+1));
+					tv1f.setLayoutParams(params);
+					tr.addView(tv1f);
+				
+					tl.addView(tr);
+					
+				}
+		        
+		        return V;
+			
 		}
 	}
 
